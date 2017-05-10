caedae54c17121442f48114823a77cc9d2c723e1
su-kun1899 <higedrum.coz@gmail.com>
2017-05-10 16:10:52 +0900

    Add post method.

--- src/main/kotlin/red/sukun1899/sskb/controller/page/MemoController.kt
+++ src/main/kotlin/red/sukun1899/sskb/controller/page/MemoController.kt
@@ -3,7 +3,10 @@ package red.sukun1899.sskb.controller.page
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import red.sukun1899.sskb.model.Memo
 
 /**
  * @author su-kun1899
@@ -18,6 +21,14 @@ class MemoController {
                 "author" to "Empty Author"
         )
         val items = listOf<Map<String, Any>>(item)
+        model.addAttribute("items", items)
+
+        return "memo"
+    }
+
+    @PostMapping
+    fun post(@ModelAttribute item: Memo, model: Model): String {
+        val items = listOf<Memo>(item)
         model.addAttribute("items", items)
 
         return "memo"
