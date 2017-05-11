7a4aeb411dc4c397c0c5793b521bbac0c8c43d39
su-kun1899 <higedrum.coz@gmail.com>
2017-05-11 17:04:29 +0900

    Add test case.

--- src/test/kotlin/red/sukun1899/sskb/controller/page/MemoControllerSpec.kt
+++ src/test/kotlin/red/sukun1899/sskb/controller/page/MemoControllerSpec.kt
@@ -5,6 +5,7 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
@@ -15,11 +16,43 @@ class MemoControllerSpec : Spek({
     given("MemoController") {
         val mockMvc = MockMvcBuilders.standaloneSetup(MemoController()).build()
 
-        on("get without params") {
+        on("get") {
             val resultActions = mockMvc.perform(get("/memo/"))
 
-            it("should return OK status") {
-                resultActions.andExpect(status().isOk())
+            it("should return ok status") {
+                resultActions.andExpect(status().isOk)
+            }
+
+            it("should have items attribute") {
+                resultActions.andExpect(model().attributeExists("items"))
+            }
+
+            it("should return memo view") {
+                resultActions.andExpect(view().name("memo"));
+            }
+        }
+
+        on("getParams with 'foo'") {
+            val resultActions = mockMvc.perform(get("/memo/param/foo"))
+
+            it("should return ok status") {
+                resultActions.andExpect(status().isOk)
+            }
+
+            it("should have items attribute") {
+                resultActions.andExpect(model().attributeExists("items"))
+            }
+
+            it("should return memo view") {
+                resultActions.andExpect(view().name("memo"));
+            }
+        }
+
+        on("post with memo model") {
+            val resultActions = mockMvc.perform(post("/memo/").param("memo", "foo").param("author", "bar"))
+
+            it("should return ok status") {
+                resultActions.andExpect(status().isOk)
             }
 
             it("should have items attribute") {
